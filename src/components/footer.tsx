46d532f1d8bbc5263621034b29d81b1937cd150f
p-karari <kararikelvin2002@gmail.com>
2025-07-24 16:05:37 +0300

    Mpdified footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <>
         <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content px-12">
             <aside>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
             </aside>
         </footer>
         </>
