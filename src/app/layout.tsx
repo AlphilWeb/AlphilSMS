c12b29c16d5740949211c7c057baa88881746954
p-karari <kararikelvin2002@gmail.com>
2025-08-13 14:48:14 +0300

    updated layout to accomodate the query client provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { QueryProvider } from "./providers";
 
 export default function RootLayout({
   children,
@@ -9,7 +10,9 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
-        {children}
+        <QueryProvider>
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
