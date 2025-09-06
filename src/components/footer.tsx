42a6c96c82cb3b022f5e2feab3d26efbc291d1e4
p-karari <kararikelvin2002@gmail.com>
2025-09-06 11:33:43 +0300

    Similar design for loading svgs throughout the project

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,10 @@
 export default function Footer() {
     return (
         <>
-        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content px-12 py-6">
+        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content px-12 py-6 text-white">
             <aside>
                 <p>Copyright © {new Date().getFullYear()} - All rights reserved</p>
+                <p>Powered by iWebX</p>
             </aside>
         </footer>
         </>
