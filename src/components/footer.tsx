f67945de75ec3226a884d9c39fb47c66bdb728e0
p-karari <kararikelvin2002@gmail.com>
2025-09-12 15:49:06 +0300

    better footer design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,10 @@
 export default function Footer() {
     return (
         <>
-        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content px-12 py-6 text-white">
-            <aside>
-                <p>Copyright © {new Date().getFullYear()} - All rights reserved</p>
-                <p>Powered by iWebX</p>
+        <footer className=" footer sm:footer-horizontal footer-center bg-base-300 text-base-content px-12 py-6 text-white">
+            <aside >
+                <p className="text-white">Copyright © {new Date().getFullYear()} - All rights reserved</p>
+                <p className="text-white">Powered by iWebX</p>
             </aside>
         </footer>
         </>
